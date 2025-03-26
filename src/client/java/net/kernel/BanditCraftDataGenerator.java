978e960533163e053aaa9d11f4e9a5801c021293
Kernel <kernelxdev@gmail.com>
2025-03-26 14:30:01 +0100

    Fabric Mod Template

---
+++ src/client/java/net/kernel/BanditCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package net.kernel;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BanditCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
